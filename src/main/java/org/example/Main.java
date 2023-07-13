b6469aff289f6a2777ea1af285121c734b870999
Buket Yurt <buketyurt01@gmail.com>
2023-07-13 16:15:01 +0300

    fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Basamak Sayısı : ");
         int step = scanner.nextInt(); //take step size
-        for (int i = step; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+
+        int StarCount = 2 * step - 1;
+        int SpaceCount = 0;
+
+        for (int i = 0; i < step; i++) {
+            for (int j = 0; j < SpaceCount; j++) {//print spaces
+                System.out.print(" ");
+            }
+            for (int k = 0; k < StarCount; k++) {//print stars
                 System.out.print("*");
             }
             System.out.println();
+
+            StarCount -= 2;
+            SpaceCount++;
         }
-
-
     }
 }
