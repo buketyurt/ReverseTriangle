cde202ed39c8f669ecca311236c1ed0d51469788
Buket Yurt <buketyurt01@gmail.com>
2023-07-13 15:51:07 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Basamak Sayısı : ");
+        int step = scanner.nextInt(); //take step size
+        for (int i = step; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
